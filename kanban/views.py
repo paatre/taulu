cf3d60ce9410cf10d8e51dc6291257d6d229e851
Teemu Viikeri <tepavi@gmail.com>
2024-06-15 16:08:04 +0300

    Add Working column and milestone info

--- kanban/views.py
+++ kanban/views.py
@@ -9,35 +9,41 @@ gl = gitlab.Gitlab(settings.GITLAB_URL, private_token=settings.GITLAB_PRIVATE_TO
 current_user = gl.users.list(username=settings.GITLAB_USERNAME)[0]
 
 
-def get_gitlab_issues(user, state=None, page=1, per_page=20):
-    issues = gl.issues.list(
-        assignee_id=user.id,
-        state=state,
-        page=page,
-        per_page=per_page,
-        order_by="updated_at",
-        sort="desc",
-        scope="all",
-        with_labels_details=True,
-    )
+def get_gitlab_issues(user, state=None, label=None, page=1, per_page=10):
+    issue_params = {
+        "assignee_id": user.id,
+        "order_by": "updated_at",
+        "page": page,
+        "per_page": per_page,
+        "scope": "all",
+        "sort": "desc",
+        "state": state,
+        "with_labels_details": True,
+    }
+
+    if label:
+        issue_params["labels"] = label
+
+    issues = gl.issues.list(**issue_params)
 
     tasks = []
     for issue in issues:
         labels = [
             {
+                "color": label["color"],
                 "name": label["name"],
                 "text_color": label["text_color"],
-                "color": label["color"],
             }
             for label in issue.labels
         ]
         tasks.append(
             {
-                "title": issue.title,
-                "description": issue.description,
-                "state": issue.state,
-                "labels": labels,
                 "author": issue.author["name"],
+                "description": issue.description,
+                "labels": labels,
+                "milestone": issue.milestone["title"] if issue.milestone else None,
+                "state": issue.state,
+                "title": issue.title,
                 "web_url": issue.web_url,
             }
         )
@@ -47,22 +53,29 @@ def get_gitlab_issues(user, state=None, page=1, per_page=20):
 
 def kanban_board(request):
     open_tasks = get_gitlab_issues(current_user, state="opened")
+    working_tasks = get_gitlab_issues(current_user, state="opened", label="Working")
     closed_tasks = get_gitlab_issues(current_user, state="closed")
 
     return render(
         request,
         "kanban/kanban.html",
-        {"open_tasks": open_tasks, "closed_tasks": closed_tasks, "page": 1},
+        {
+            "closed_tasks": closed_tasks,
+            "open_tasks": open_tasks,
+            "page": 1,
+            "working_tasks": working_tasks,
+        },
     )
 
 
 def load_more_issues(request):
     state = request.GET.get("state")
+    label = request.GET.get("label")
     page = int(request.GET.get("page", 1))
-    tasks = get_gitlab_issues(current_user, state=state, page=page)
+    tasks = get_gitlab_issues(current_user, state=state, label=label, page=page)
 
     return render(
         request,
         "kanban/issues_partial.html",
-        {"tasks": tasks, "state": state, "page": page},
+        {"tasks": tasks, "state": state, "label": label, "page": page},
     )
