110170e5e24c13903531e7b1c44d3c1b52286bd1
Teemu Viikeri <tepavi@gmail.com>
2024-09-16 21:50:52 +0300

    Update copy.sh script

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-cat kanban/views.py kanban/models.py kanban/admin.py kanban/management/**/*.py kanban/templates/kanban/*.html | wl-copy
+cat kanban/admin.py \
+    kanban/gitlab.py \
+    kanban/management/**/*.py \
+    kanban/models.py \
+    kanban/templates/**/*.html \
+    kanban/views.py \
+    kanban/tests.py \
+    project/settings.py \
+    project/urls.py \
+    | wl-copy
